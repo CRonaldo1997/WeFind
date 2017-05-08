26f4b5b0f71a686127cc209cd61af8b762a49df6
meng <mz.zhoumeng@gmail.com>
2017-05-08 02:28:28 -0700

    MZ: Now able to sign up without saving profile picture.

--- app/src/main/java/com/example/c_ronaldo/we_finder/SignUpActivity.java
+++ app/src/main/java/com/example/c_ronaldo/we_finder/SignUpActivity.java
@@ -235,10 +235,6 @@ public class SignUpActivity extends AppCompatActivity {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         Log.d(TAG, "createUserWithEmail:onComplete:" + task.isSuccessful());
-//                        String username = userName.getText().toString();
-//                        String email = signUpEmail.getText().toString();
-//                        uploadToFirebase(username, email, year, zodiacSign, gender, imageUri);
-
                         // If sign in fails, display a message to the user. If sign in succeeds
                         // the auth state listener will be notified and logic to handle the
                         // signed in user can be handled in the listener.
